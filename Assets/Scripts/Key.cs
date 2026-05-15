20f4cdbc8d509f56340886f560187c21090d9867
BenjaminBaredes <bbaredes@gmail.com>
2026-05-15 14:31:57 -0300

    .

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -4,7 +4,22 @@ using UnityEngine;
 
 public class Key : MonoBehaviour
 {
+
+bool pressed = false;
+float pressedOffset = 1f;
+
    void OnMouseDown(){
     Debug.Log("Click en " + gameObject.name);
+    transform.Translate(0, -1, 0);
+   }
+
+   void OnMouseUp(){
+    if(pressed){
+        transform.Translate(0, pressedOffset, 0);
+    }
+   }
+
+   void OnMouseExit(){
+    
    }
 }
